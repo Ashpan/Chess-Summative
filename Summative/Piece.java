9d00246b7d0eeda459009e4fb5127da690e8fefd
omarthen3rd <omarehsaan2@gmail.com>
2018-06-05 13:45:42 -0400

    added Piece class

---
+++ Summative/Piece.java
@@ -0,0 +1,17 @@
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Piece {
+
+    private Image pieceImage;
+    public String[][] boardArray = new String[8][8];
+
+    Piece(Image image) {
+
+        this.pieceImage = image;
+
+    }
+
+
+
+}
