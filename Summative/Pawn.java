a16e1617cc045f130881f4da3a187888c98aec06
Ashpan Raskar <ashpanr@gmail.com>
2018-06-05 13:48:14 -0400

    pawn constructor

--- Summative/Pawn.java
+++ Summative/Pawn.java
@@ -1,2 +1,16 @@
-public class Pawn {
+public class Pawn extends Piece{
+    Pawn(int xCoord, int yCoord){
+        Piece.boardArray[yCoord][xCoord] = "P";
+    }
+    public void methodOne(){}
+    public void methodTwo(){}
+
 }
+
+
+B
+K
+N
+P
+Q
+R
